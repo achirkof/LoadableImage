0d1fd5701487ef25f65f23080c9e6185e794b361
Andrey Chirkov <achirkof@gmail.com>
2020-03-01 14:33:27 +0300

    Fix Package config

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [],
     targets: [
         Target.target(name: "LoadableImage", dependencies: [], exclude: ["Example"]),
-        Target.testTarget(name: "LoadableImage", dependencies: [])
+        Target.testTarget(name: "LoadableImageTests", dependencies: [])
     ],
     swiftLanguageVersions: [SwiftVersion.v5]
 )
