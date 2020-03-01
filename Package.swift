aadb666e51e88023b0c02c4d39f52e18888e21c0
Andrey Chirkov <achirkof@gmail.com>
2020-03-01 13:53:21 +0300

    Fix Travis config

--- Package.swift
+++ Package.swift
@@ -6,28 +6,14 @@ import PackageDescription
 let package = Package(
     name: "LoadableImage",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "LoadableImage",
-            targets: ["LoadableImage"]),
+        .library(name: "LoadableImage", targets: ["LoadableImage"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LoadableImage",
-            dependencies: []),
-        .testTarget(
-            name: "LoadableImageTests",
-            dependencies: ["LoadableImage"]),
+        .target(name: "LoadableImage", dependencies: []),
+        .testTarget(name: "LoadableImageTests", dependencies: ["LoadableImage"]),
     ],
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     exclude: ["Example"],
     swiftLanguageVersions: [.v5]
 )
