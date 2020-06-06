478d923e7f3249b617fedbac78a9b21ebddf40d9
Andrey <achirkof@gmail.com>
2020-06-06 23:49:06 +0300

    Extract AssetsProvider

---
+++ Sources/LoadableImage/Loadable/NetworkProvider.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by CHIRKOV Andrey on 06.06.2020.
+//
+
+import Foundation
--- Sources/LoadableImage/Loadable/UIImageLoadable.swift
+++ Sources/LoadableImage/Loadable/UIImageLoadable.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
-//  
+//  UIImageLoadable.swift
 //
 //  Created by CHIRKOV Andrey on 30.05.2020.
 //
 
-import Foundation
+import Combine
+import UIKit
+
+public class UIImageLoadable: Loadable {
+    private let image: UIImage?
+    private let assets: AssetsProvider
+
+    public init(
+        image: UIImage?,
+        assets: AssetsProvider = Assets()
+    ) {
+        self.image = image
+        self.assets = assets
+    }
+
+    public func load() -> AnyPublisher<UIImage, ImageLoadError> {
+        guard let image = image else {
+            return Fail<UIImage, ImageLoadError>(error: .notExists)
+                .eraseToAnyPublisher()
+        }
+
+        return assets.publisher(for: image)
+            .setFailureType(to: ImageLoadError.self)
+            .eraseToAnyPublisher()
+    }
+
+    public func cancel() {}
+}
